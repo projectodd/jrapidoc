0779d63820c462a30d3d157c6c8455142531aa3d
sarzwest <jiratom@seznam.cz>
2015-09-28 21:44:34 +0200

    removed comment

--- jrapidoc-soap-plugin/src/test/java/airservice/entity/destination/D1.java
+++ jrapidoc-soap-plugin/src/test/java/airservice/entity/destination/D1.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * Created by papa on 25.4.15.
  */
-public class D1<T1 extends Destination & ObjectInterface, T2 extends T1>{//proto getbounds  muze vracet vicero ohraniceni
+public class D1<T1 extends Destination & ObjectInterface, T2 extends T1>{
 
     public T1 destination;
     public T2 object;
